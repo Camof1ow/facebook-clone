a9e0d1290b53ed880e21aadd1983b68b52297e26
G-Hae <sifialee@gmail.com>
2022-08-22 14:38:45 +0900

    로그인/회원가입/db.json/router/yarn lock 등

--- src/route/router.js
+++ src/route/router.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mainpage from "../pages/Mainpage";
+import LoginPage from "../pages/Loginpage";
+import SignUp from "../pages/Signuppage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Mainpage />}></Route>
+        <Route path='login' element={<LoginPage/>}/>
+        <Route path='signup' element={<SignUp/>}/>
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default Router;
+
+
