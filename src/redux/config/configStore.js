adfb8275258c8e3cb580dfc8de5e560731775aaf
G-Hae <sifialee@gmail.com>
2022-08-24 00:01:26 +0900

    친구리스트 불러오기

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import comments from "../modules/commentSlice";
 import facebookPosts from "../modules/postSlice";
 import userInfo from "../modules/userinfoSlice";
+import friends from "../modules/friendsSlice";
 
 export const store = configureStore({
   reducer: {
     facebookPosts: facebookPosts,
     userInfo: userInfo,
     comments: comments,
+    friends: friends,
   },
 });
